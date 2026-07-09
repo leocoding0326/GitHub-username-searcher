6eef171959a2cb23f9639c2a28afb29d58f11698
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-08 23:13:47 -0500

    Initialized Vite and modified app.jsx to show
    h1 with text username Checker,
    added a form with an action of submit and an input with
    a type of text and a placeholder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Username Checker</h1>
+      <form action="submit">
+        <input type="text" placeholder='Enter Username'/>
+      </form>
+    </div>
+  );
+}
+
+export default App
