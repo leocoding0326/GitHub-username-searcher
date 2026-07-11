972a2eb4139303674efaf578fb662ea0dc9b84fc
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-10 22:37:33 -0500

    fixed query string replaced : for =

--- src/API/userSearch.js
+++ src/API/userSearch.js
@@ -2,6 +2,6 @@ const userSearch = async (userInput) => {
     const baseUrl = 'https://api.github.com/';
     const endPoint = 'https://api.github.com/';
     const input = userInput;
-    const query = `q=${encodeURIComponent(input)}&per_page=5`;
+    const query = `?q=${encodeURIComponent(input)}&per_page=5`;
     
 }
