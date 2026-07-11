f6da0f21bf666bea3ba73cfca241111911c2ee5b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-10 22:34:28 -0500

    Stored base url, endpoint, input and query in var

--- src/API/userSearch.js
+++ src/API/userSearch.js
@@ -1,3 +1,7 @@
-const userSearch = async () => {
-
+const userSearch = async (userInput) => {
+    const baseUrl = 'https://api.github.com/';
+    const endPoint = 'https://api.github.com/';
+    const input = userInput;
+    const query = `q=${encodeURIComponent(input)}&per_page=5`;
+    
 }
