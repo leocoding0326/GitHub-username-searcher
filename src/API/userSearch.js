13e4c836f5b20479897e31fb168e572b8ca360eb
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-10 22:49:37 -0500

    Fetched api and also returned whole json object
    placed it inside a try catch that will show the error in the console
    if catch is trigger,
    
    added a guard clausule that will show error in console if !response

--- src/API/userSearch.js
+++ src/API/userSearch.js
@@ -1,6 +1,19 @@
 const userSearch = async (userInput) => {
-    const baseUrl = 'https://api.github.com/';
+    const baseUrl = 'https://api.github.com';
     const endPoint = '/search/users';
     const query = `?q=${encodeURIComponent(userInput)}&per_page=5`;
     const urlToFetch = baseUrl + endPoint + query;
+    
+    try {
+        const response = await fetch(urlToFetch);
+        if(!response.ok) {
+            console.log('Theres been an error')
+            return
+        }
+        const result = await response.json();
+        console.log(result);
+
+    } catch (error) {
+        console.log(error)
+    }
 }
