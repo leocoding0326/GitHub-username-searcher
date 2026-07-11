797ea5c07b53e5fa34402ac2959208d383309dde
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-10 22:38:15 -0500

    Updated endpoint to /search/users

--- src/API/userSearch.js
+++ src/API/userSearch.js
@@ -1,7 +1,6 @@
 const userSearch = async (userInput) => {
     const baseUrl = 'https://api.github.com/';
-    const endPoint = 'https://api.github.com/';
+    const endPoint = '/search/users';
     const input = userInput;
     const query = `?q=${encodeURIComponent(input)}&per_page=5`;
-    
 }
