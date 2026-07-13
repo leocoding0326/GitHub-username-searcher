8a430a62cecd10147d4e2b8a9ded3758731ab262
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-12 22:14:37 -0500

    Imported userSearch into app.jsx; called component AutoComplete with a prop passing userSearch into its child

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import userSearch from './API/userSearch.js';
 
 const App = () => {
   return (
     <div>
       <h1>Username Checker</h1>
-      <form action="submit">
+        <form action="submit">
         <input type="text" placeholder='Enter Username'/>
+        <AutoComplete userObject={userSearch}/>
         <input type="button" value="Search" />
       </form>
     </div>
