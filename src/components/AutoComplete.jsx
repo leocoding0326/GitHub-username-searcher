f5bf0b1cd683486553ff339fdd1fafb8903c226e
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-12 22:24:19 -0500

    Wrote function to handle change on input:
    it will run my api function and pass the input value as parameter
    called on change in input

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,10 @@
 const AutoComplete = ({searchFunction}) => {
-    
-}
+    const handleChange = (e) => {
+       console.log(searchFunction(e.target.value));
+    }
+    return (
+        <input onChange={handleChange}/>
+    );
+};
+
+export default AutoComplete;
