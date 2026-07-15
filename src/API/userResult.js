cd5d863b3d77609b67a37ac8a940552818d2b183
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-15 14:57:41 -0500

    New api fetch request created:
    fetch github users endpoint;
    receive an input and use it to fetch the username;
    returns a json object;
    exported it to use in components

--- src/API/userResult.js
+++ src/API/userResult.js
@@ -1,3 +1,17 @@
 const userSearch = async (inputSubmitted) => {
-    
+    const baseUrl = 'https://api.github.com';
+    const endPoint = `/users/${inputSubmitted}`;
+    const urlToFecth = baseUrl + endPoint;
+
+    try {
+        const response = await fetch(urlToFecth);
+
+        const result = await response.json();
+        return result
+
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+export default userSearch;
