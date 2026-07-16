e773df98668ad04bb54af6d37ca8a26e1ad21810
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-15 23:04:03 -0500

    Changed loading bar to render as an li instead of replace the whole ul component

--- src/components/SuggestionList.jsx
+++ src/components/SuggestionList.jsx
@@ -1,14 +1,20 @@
 import LoadingBar from './LoadingBar'
 
-const SuggestionList = ({data=[], dataHandler, isLoading, handleClick}) => {
-
+const SuggestionList = ({ data = [], dataHandler, isLoading, handleClick }) => {
     return (
-            isLoading ? <LoadingBar /> : 
-            <ul>
-                {data.map((item, index) => 
-                    <li key={item.id ?? index} onClick={()=>handleClick(item)}>{dataHandler(item)}</li>
-                )}
-            </ul>
+        <ul className="suggestion-list">
+            {isLoading ? (
+                <li className="loading">
+                    <LoadingBar />
+                </li>
+            ) : (
+                data.map((item) => (
+                    <li key={item.id} onClick={() => handleClick(item)}>
+                        {dataHandler(item)}
+                    </li>
+                ))
+            )}
+        </ul>
     );
 };
 
