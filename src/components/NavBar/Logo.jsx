fc14cd6e0b0ccf669f6c159d643ad4dba6723b95
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 16:34:51 -0500

    Syled logo component and added span element

--- src/components/NavBar/Logo.jsx
+++ src/components/NavBar/Logo.jsx
@@ -1,8 +1,8 @@
 const Logo = ({imgSrc, altText}) => {
     return (
-        <div className="max-w-full h-full flex ">
-            <img src={imgSrc} alt= {altText} className="h-10 w-auto"/>
-            <h1>GitHub Finder</h1>
+        <div className="max-w-full h-full flex gap-2 items-center">
+            <img src={imgSrc} alt= {altText} className="max-h-7 w-auto -scale-x-100"/>
+            <h1 className="font-bold text-3xl tracking-wide">Github<span className="text-digital-blue-500">Finder</span></h1>
         </div>
     )
 };
