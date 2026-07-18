f6e9b4ecc9304c4b68eee7c518193ccfdf2e3812
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 11:11:27 -0500

    Installed and tested tailwind css; deleted app.css as it is not needed, wrote tailwind plugin inside vite config,

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import './App.css';
 import userSearch from './api/userSearch.js';
 import SearchAutoComplete from './components/SearchAutoComplete.jsx';
 import Results from './components/Results.jsx';
