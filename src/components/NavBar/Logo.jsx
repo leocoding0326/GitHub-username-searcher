92d2bfc8fa30a58ff45fff3d3693152399d2fcb0
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 15:50:53 -0500

    Displayed logo and text in logo component:
    imported img into navbar component and passed down to logo as a prop;
    called the prop as img src in logo as well as alt

--- src/components/NavBar/Logo.jsx
+++ src/components/NavBar/Logo.jsx
@@ -1,5 +1,10 @@
-const Logo = () => {
-
+const Logo = ({imgSrc, altText}) => {
+    return (
+        <div>
+            <img src={imgSrc} alt= {altText}/>
+            <h1>GitHub Finder</h1>
+        </div>
+    )
 };
 
 export default Logo;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
+import logo from "../../assets/logo.svg";
 
 const NavBar = () => {
     return (
         <div>
-            <Logo />
+            <Logo imgSrc = {logo} altText="github finder logo"/>
             <NavLinks />
         </div>
     )
