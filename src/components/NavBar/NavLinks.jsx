e0046d0cbf0024a56a55c702ceb7126c89c40f69
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 18:37:26 -0500

    Created nav links

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,5 +1,11 @@
 const NavLinks = () => {
-
+    return (
+        <ul>
+            <li><a href=""></a>Tools</li>
+            <li><a href=""></a>Why</li>
+             <li><a href=""></a>How To</li>
+        </ul>
+    )
 };
 
 export default NavLinks;
