a622bcbce61af1c2e3a703dd3782ace9bd2a34e5
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-19 00:46:49 -0500

    Completed toggle icon: used span to create 3 lines; created an useState to hold open state; create fuction that checks to oposit of open state when invoqued, added function to burger menu element,

--- src/components/NavBar/BurgerMenu.jsx
+++ src/components/NavBar/BurgerMenu.jsx
@@ -1,10 +1,14 @@
-const BurgerMenu = () => {
+const BurgerMenu = ({helperFunction, open}) => {
+    
+
     return (
-        <div className="relative h-5 w-6">
-            <span className="absolute top-0 h-0.5 w-6 bg-black rounded-full"></span>
-            <span className="absolute top-2 h-0.5 w-6 bg-black rounded-full"></span>
-            <span className="absolute top-4 h-0.5 w-6 bg-black rounded-full"></span>
-        </div>
+        <button onClick={helperFunction}>
+            <div className="relative h-5 w-6">
+                <span className={`absolute top-0 h-0.5 w-6 bg-black rounded-full transition-all duration-300 ease-in-out ${open ? "top-2 rotate-45" : "top-0 rotate-0"}`}></span>
+                <span className={`absolute top-2 h-0.5 w-6 bg-black rounded-full transition-all duration-300 ease-in ${open ? 'opacity-0 scale-x-0': 'opacity-100 scale-x-100'}`}></span>
+                <span className={`absolute top-2 h-0.5 w-6 bg-black rounded-full transition-all duration-300 ease-in-out ${open ? 'top-0 -rotate-45 ':'top-4 rotate-0'}`}></span>
+            </div>
+        </button>
     );
 };
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import NavAuth from "./NavAuth";
@@ -5,15 +6,21 @@ import logo from "../../assets/logo.svg";
 import BurgerMenu from "./BurgerMenu";
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleToggle = () => {
+      setOpen(prev => !prev);
+    }
+    
     return (
-        <div className="max-w-[1440px] h-14 px-6 my-2 flex justify-between m-auto gap-2">
+        <div className="max-w-[1440px] h-14 px-8 my-2 flex justify-between m-auto gap-2">
             <Logo imgSrc = {logo} altText="github finder logo"/>
             <div className="hidden md:flex ml-auto gap-4 flex-wrap-reverse">
                 <NavLinks />
                 <NavAuth />
             </div>
             <div className="md:hidden flex items-center">
-                <BurgerMenu />
+                <BurgerMenu helperFunction={handleToggle} open ={open}/>
             </div>
         </div>
     )
