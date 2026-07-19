c61b032dbeb8717b2a7a28d371e77530489aad00
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 20:37:32 -0500

    Styled sign up button:
    added display flex to parent and gap of 2

--- src/components/NavBar/NavAuth.jsx
+++ src/components/NavBar/NavAuth.jsx
@@ -1,8 +1,8 @@
 const NavAuth = () => {
     return (
-        <div className="[&>button]:px-4 [&>button]:py-1">
+        <div className="[&>button]:px-4 [&>button]:py-1 flex gap-2">
             <button className="text-digital-blue-500 !border rounded-md hover:bg-digital-blue-50 transition-colors">Log In</button>
-            <button>Sign Up</button>
+            <button className="bg-digital-blue-500 rounded-md hover:bg-digital-blue-700 text-white !border transition-colors">Sign Up</button>
         </div>
     )
 }
