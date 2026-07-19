c8886a51935697368dddd7370d7ff9942f30487b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-19 18:16:49 -0500

    NavAuth added to slide menu,
    set slide menu to flex-col,
    space around and items-center

--- src/components/NavBar/SlideMenu.jsx
+++ src/components/NavBar/SlideMenu.jsx
@@ -1,10 +1,12 @@
 import NavLinks from "./NavLinks";
+import NavAuth from "./NavAuth";
 
 const SlideMenu = ({open}) => {
     return (
-        <div className={`transition-all duration-300 right-0 top-0 fixed h-[90vh] w-screen rounded-md shadow-sm ${!open ? 'translate-x-full' : 'translate-x-0'
+        <div className={`flex flex-col justify-around items-center transition-all duration-300 right-0 top-0 fixed h-[90vh] w-screen rounded-md shadow-sm ${!open ? 'translate-x-full' : 'translate-x-0'
         }`}>
-          <NavLinks />  
+          <NavLinks />
+          <NavAuth />
         </div>
     )
 }
