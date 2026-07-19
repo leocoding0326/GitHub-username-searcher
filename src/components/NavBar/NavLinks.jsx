bc4cce96cc75c65907f4cdb24047786896e8b510
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-18 19:01:51 -0500

    Completed styling of NavLinks:
    Targeted the anchors from the ul to avoid repeating classes;
    change text to gray,
    hover state to text color white and bg blue;
    added padding,
    rounded corners,
    added a transition delay of 500ms,
    changed display of anchors to inline block

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,9 +1,19 @@
 const NavLinks = () => {
     return (
-        <ul>
-            <li><a href=""></a>Tools</li>
-            <li><a href=""></a>Why</li>
-             <li><a href=""></a>How To</li>
+        <ul className="flex gap-3 items-center
+        [&>li>a]:text-gray-500 
+        [&>li>a]:hover:bg-digital-blue-400 
+        [&>li>a]:hover:text-white
+        [&>li>a]:px-2
+        [&>li>a]:py-1
+        [&>li>a]:rounded-md
+        [&>li>a]:transition-colors
+        [&>li>a]:duration-500
+        [&>li>a]:inline-block
+        ">
+            <li><a href="">Tools</a></li>
+            <li><a href="">Why</a></li>
+            <li><a href="">How To</a></li>
         </ul>
     )
 };
