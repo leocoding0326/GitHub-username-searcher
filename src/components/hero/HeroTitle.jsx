53e83fe794bcfe81962dd1da81d01d0b564e6933
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-20 07:06:51 -0500

    Margin and space added in HeroTitle:
    space-x set to 2 as well as mb

--- src/components/hero/HeroTitle.jsx
+++ src/components/hero/HeroTitle.jsx
@@ -1,6 +1,6 @@
 const HeroTitle = () => {
     return (
-        <div>
+        <div className="space-y-2 mb-2">
             <h2 className="font-bold text-3xl tracking-wide text-center">Search <span className="text-digital-blue-500">GitHub</span> Users</h2>
             <p>Find any GitHub user and explore their profile, repos and more.</p>
         </div>
