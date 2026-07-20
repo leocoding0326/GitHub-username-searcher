b9e1dc5b8db22b5cd4249a0a0945d6192c36a7eb
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-20 17:13:13 -0500

    Styling added to ul:
    used shadow instead of border to fix ul,
    p of 5 added to li as well as hover effect of bg,
    span added inside li with dotted border

--- src/components/hero/SuggestionList.jsx
+++ src/components/hero/SuggestionList.jsx
@@ -2,15 +2,16 @@ import LoadingBar from './LoadingBar'
 
 const SuggestionList = ({ data = [], dataHandler, isLoading, handleClick, search, selectUser }) => {
     return (
-        <ul className='min-h-[50%] absolute'>
+        <ul className='absolute bg-white w-[50%] [&>li]:p-5 shadow-sm transition-all rounded-md'>
             {isLoading ? (
                 <li className="loading">
                     <LoadingBar />
                 </li>
             ) : data.length > 0 ? (
                 data.map((item) => (
-                    <li key={item.id} onClick={() => handleClick(item)}>
+                    <li key={item.id} onClick={() => handleClick(item)} className='flex items-center hover:bg-digital-blue-100 transition-colors rounded-md'>
                         {dataHandler(item)}
+                        <span className='flex-1 border-dotted border-b border-gray-200 mx-2'></span>
                     </li>
                 ))
             ) : search !== '' && !selectUser ? (
