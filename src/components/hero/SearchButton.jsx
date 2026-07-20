e3bbc2130c883d1cf7ecbbbf0a0693fcfb05b60b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-20 16:41:39 -0500

    set main width to 80%

--- src/components/hero/SearchButton.jsx
+++ src/components/hero/SearchButton.jsx
@@ -3,7 +3,7 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchButton = () => {
     return  (
-        <button type='submit' className='bg-digital-blue-500 rounded-md px-2 py-1'>
+        <button type='submit' className='bg-digital-blue-500 rounded-md px-2 py-1 text-white'>
             <FontAwesomeIcon icon={faMagnifyingGlass}/>
             Search
         </button>
