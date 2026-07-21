b4c1049b70f20c66de1943c34c7f07c7336ba3c1
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-21 18:47:48 -0500

    Code prose added to discover text

--- src/components/Discover/DiscoverTexts.jsx
+++ src/components/Discover/DiscoverTexts.jsx
@@ -1,7 +1,7 @@
 const DiscoverText = () => {
     return (
-        <div className="mt-2">
-            <h3 className="font-bold text-2xl">Start Your Search</h3>
+        <div className="mt-2 max-w-prose">
+            <h3 className="font-bold text-2xl mb-2">Start Your Search</h3>
             <p>Enter a GitHub username above to find user profiles, repositories, statistics and more.</p>
         </div>
     );
