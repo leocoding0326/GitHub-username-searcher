c04e33d658664cca609a3639a3d9d2c631cde424
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-22 17:55:25 -0500

    Styled popular usernames to fit ui

--- src/components/Discover/PopularSearches.jsx
+++ src/components/Discover/PopularSearches.jsx
@@ -1,12 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+
 const PopularSearches = ({popularArray}) => {
     return (
-        <div>
-        <h3>Popular Searches</h3>
-        <ul>
-            {popularArray.map(user => (
-  <li key={user}>{user}</li>
-))}
-        </ul>
+        <div className="w-full pb-4 flex flex-col gap-6">
+            <h4 className="font-bold text-xl">Popular Searches</h4>
+            <ul className="flex justify-around flex-wrap gap-4">
+                {popularArray.map(user => (
+                <li key={user} className="inline-block bg-digital-blue-100 p-2 rounded-md border border-digital-blue-200"><FontAwesomeIcon icon={faUser} style={{color: '#0066FF'}}/> {user}</li>
+                ))}
+            </ul>
         </div>
     );
 };
