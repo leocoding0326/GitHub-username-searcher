3b471a03f42b7e1e18f592f94a02a62778cea8d6
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-23 22:30:21 -0500

    Added pointer effect to auth buttons

--- src/components/NavBar/NavAuth.jsx
+++ src/components/NavBar/NavAuth.jsx
@@ -1,6 +1,6 @@
 const NavAuth = () => {
     return (
-        <div className="md:[&>button]:px-4 [&>button]:px-10 md:[&>button]:py-1 [&>button]:py-3 flex flex-wrap justify-center md:gap-2 gap-4 items-center">
+        <div className="md:[&>button]:px-4 [&>button]:px-10 md:[&>button]:py-1 [&>button]:py-3 flex flex-wrap justify-center md:gap-2 gap-4 items-center [&>button]:cursor-pointer">
             <button className="text-digital-blue-400 !border rounded-md hover:bg-digital-blue-50 transition-colors">Log In</button>
             <button className="bg-digital-blue-400 rounded-md hover:bg-digital-blue-700 text-white !border !border-digital-blue-400 transition-colors">Sign Up</button>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
     
     return (
     <nav className="w-full bg-digital-blue-900">
-        <div className="max-w-[1440px] mx-auto h-14 px-6 flex justify-between items-center gap-2">
+        <div className="max-w-360 mx-auto h-14 px-6 flex justify-between items-center gap-2">
             <Logo imgSrc={logo} altText="github finder logo" />
 
             <div className="hidden md:flex gap-4">
