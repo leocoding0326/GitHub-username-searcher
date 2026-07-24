0205f2eefba37938e6b13ace10097dc66a5f695d
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-23 19:53:43 -0500

    Added hover state to popular searches for accessi- bility

--- src/components/Discover/PopularSearches.jsx
+++ src/components/Discover/PopularSearches.jsx
@@ -7,7 +7,7 @@ const PopularSearches = ({popularArray}) => {
             <h4 className="font-bold text-xl">Popular Searches</h4>
             <ul className="flex justify-around flex-wrap gap-4">
                 {popularArray.map(user => (
-                <li key={user} className="inline-block bg-digital-blue-100 p-2 rounded-md border border-digital-blue-200"><FontAwesomeIcon icon={faUser} style={{color: '#0066FF'}}/> {user}</li>
+                <li key={user} className="inline-block p-2 rounded-md border border-digital-blue-200 text-[#0066FF] hover:bg-digital-blue-50 transition-colors cursor-pointer"><FontAwesomeIcon icon={faUser} style={{color: '#0066FF'}}/> {user}</li>
                 ))}
             </ul>
         </div>
