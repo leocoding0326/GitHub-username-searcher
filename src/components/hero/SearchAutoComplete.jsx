e829f2fe489124dda16bc5ca038c192698c28597
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-23 22:51:21 -0500

    New useRef added to clear ul when click outside:
    ran it inside useEffect so it runs after render,

--- src/components/hero/SearchAutoComplete.jsx
+++ src/components/hero/SearchAutoComplete.jsx
@@ -10,6 +10,7 @@ const SearchAutoComplete = ({searchFunction, getLabel, clearResult, search, setS
     const [loading, setLoading] = useState(false);
     const [hasTyped, setHasTyped] = useState(false)
     const inputRef = useRef(null)
+    const searchContainerRef = useRef(null);
     
     const debouncedSearch = useMemo(
         () => 
@@ -66,8 +67,26 @@ const SearchAutoComplete = ({searchFunction, getLabel, clearResult, search, setS
         setHasTyped(false);
     }
 
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (
+                searchContainerRef.current &&
+                !searchContainerRef.current.contains(e.target)
+            ) {
+                setUsers([]);
+                setHasTyped(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className='sm:h-[40vh] h-[60vh] flex flex-col justify-center sm:min-w-[80%] mx-4'>
+        <div ref={searchContainerRef} className='sm:h-[40vh] h-[60vh] flex flex-col justify-center sm:min-w-[80%] mx-4'>
             <HeroTitle />
             <div>
             <SearchBar 
