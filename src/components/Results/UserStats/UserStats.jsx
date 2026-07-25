2eaa5676092a304953b7dc7568e266d931dd58dc
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 17:09:36 -0500

    Import favicon library and all icons needed

--- src/components/Results/UserStats/UserStats.jsx
+++ src/components/Results/UserStats/UserStats.jsx
@@ -1,3 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons/faCodeBranch";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
+
 const UserStats = ({followers, following, repos, gists, org}) => {
     return (
         <ul>
