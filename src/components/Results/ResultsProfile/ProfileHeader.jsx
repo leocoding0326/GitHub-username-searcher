60ecccf8ca1bc3a67fb4a5b1e31a62db245528a9
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 16:19:27 -0500

    Div wrapper added to profile img, set w to 128px and h to auto.

--- src/components/Results/ResultsProfile/ProfileHeader.jsx
+++ src/components/Results/ResultsProfile/ProfileHeader.jsx
@@ -1,7 +1,9 @@
 const ProfileHeader = ({img, profileName, username, bio='No Bio Fund'}) => {
     return (
         <div>
-            <img src={img} alt={bio} />
+            <div className="w-28 h-auto">
+                <img src={img} alt={bio} className="w-full h-auto rounded-full"/>
+            </div>
             <h4>{profileName}</h4>
             <h5>@{username}</h5>
             <p>{bio || 'User has no bio yet'}</p>
