21628766510b026dbe373184e3f66381e9da3a6a
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 16:27:49 -0500

    Used flex to center align ProfileHeader

--- src/components/Results/ResultsProfile/ProfileHeader.jsx
+++ src/components/Results/ResultsProfile/ProfileHeader.jsx
@@ -1,11 +1,11 @@
 const ProfileHeader = ({img, profileName, username, bio='No Bio Fund'}) => {
     return (
-        <div>
+        <div className="flex flex-col items-center gap-1">
             <div className="w-28 h-auto">
                 <img src={img} alt={bio} className="w-full h-auto rounded-full"/>
             </div>
             <h4 className="text-xl font-bold text-black">{profileName}</h4>
-            <h5 className="text-sm">@{username}</h5>
+            <h5 className="text-sm font-bold">@{username}</h5>
             <p>{bio || 'User has no bio yet'}</p>
         </div>
     );
