61041b68b769484f2b68251d826c2056c66f59f1
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 16:23:29 -0500

    Set username text to small, name text to xl and font bold

--- src/components/Results/ResultsProfile/ProfileHeader.jsx
+++ src/components/Results/ResultsProfile/ProfileHeader.jsx
@@ -4,8 +4,8 @@ const ProfileHeader = ({img, profileName, username, bio='No Bio Fund'}) => {
             <div className="w-28 h-auto">
                 <img src={img} alt={bio} className="w-full h-auto rounded-full"/>
             </div>
-            <h4>{profileName}</h4>
-            <h5>@{username}</h5>
+            <h4 className="text-xl font-bold text-black">{profileName}</h4>
+            <h5 className="text-sm">@{username}</h5>
             <p>{bio || 'User has no bio yet'}</p>
         </div>
     );
