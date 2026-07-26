a1fcad7fbf3546a14d62fe50524863fa885e4f82
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 23:47:59 -0500

    Centered profile buio text using text center

--- src/components/Results/ResultsProfile/ProfileHeader.jsx
+++ src/components/Results/ResultsProfile/ProfileHeader.jsx
@@ -1,12 +1,12 @@
 const ProfileHeader = ({img, profileName, username, bio='No Bio Fund'}) => {
     return (
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-2 text-center">
             <div className="w-28 h-auto">
                 <img src={img} alt={bio} className="w-full h-auto rounded-full"/>
             </div>
             <h4 className="text-xl font-bold text-black">{profileName}</h4>
             <h5 className="text-sm font-bold">@{username}</h5>
-            <p>{bio || 'User has no bio yet'}</p>
+            <p className="py-2">{bio || 'User has no bio yet'}</p>
         </div>
     );
 };
