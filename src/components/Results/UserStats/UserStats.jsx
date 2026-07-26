a3ba32cc75bc8c467f18ba9bddce6ce98fc22334
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 23:46:01 -0500

    Ul removed in UserStats and change it to div, added separator using div and kept semmantics

--- src/components/Results/UserStats/UserStats.jsx
+++ src/components/Results/UserStats/UserStats.jsx
@@ -6,43 +6,47 @@ import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 const UserStats = ({followers, following, repos, gists, org}) => {
     return (
-        <ul className="flex p-4 justify-around bg-slate-50 border border-digital-blue-100 rounded-md shadow-sm">
-            <li className="flex items-center gap-4">
+        <div className="flex p-4 justify-around bg-slate-50 border border-digital-blue-100 rounded-md shadow-sm items-center">
+            <div className="flex items-center gap-4">
                 <FontAwesomeIcon icon={faUsers} className="text-digital-blue-400 text-2xl pb-2"/>
                 <div>
                     <p className="font-bold text-xl">{followers}</p>
                     <p className="text-gray-400 text-sm">Followers</p>
                 </div>
-            </li>
-            <li className="flex items-center gap-4">
+            </div>
+            <div className="h-10 w-px bg-zinc-300"></div>
+            <div className="flex items-center gap-4">
                 <FontAwesomeIcon icon={faUsers} className="text-purple-400 text-2xl pb-2"/>
                 <div>
                     <p className="font-bold text-xl">{following}</p>
                     <p className="text-gray-400 text-sm">Following</p>
                 </div>
-            </li>
-            <li className="flex items-center gap-4">
+            </div>
+            <div className="h-10 w-px bg-zinc-300"></div>
+            <div className="flex items-center gap-4">
                 <FontAwesomeIcon icon={faCodeBranch} className="text-pink-400 text-2xl pb-2"/>
                 <div>
                 <p className="font-bold text-xl">{repos}</p>
                 <p className="text-gray-400 text-sm">Repositories</p>
                 </div>
-            </li>
-            <li className="flex items-center gap-4">
+            </div>
+            <div className="h-10 w-px bg-zinc-300"></div>
+            <div className="flex items-center gap-4">
                 <FontAwesomeIcon icon={faStar} className="text-yellow-400 text-2xl pb-2"/>
                 <div>
                 <p className="font-bold text-xl">{gists}</p>
                 <p className="text-gray-400 text-sm">Gists</p>
                 </div>
-            </li>
-            <li className="flex items-center gap-4">
+            </div>
+            <div className="h-10 w-px bg-zinc-300"></div>
+            <div className="flex items-center gap-4">
                 <FontAwesomeIcon icon={faBuilding} className="text-green-400 text-2xl pb-2"/>
                 <div>
                 <p className="font-bold text-xl">{org || 'N/A'}</p>
                 <p className="text-gray-400 text-sm">Organizations</p>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
     );
 };
 
