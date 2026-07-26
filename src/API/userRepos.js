8fce4fa7437b63c3efa290f79dd3ec7bac981a9c
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-26 17:20:40 -0500

    Completed fetching the new api request :
    didnt user try catch as ill handle this in the component,
    !response.ok will trhow error so i can handle state in component call,
    will update all the other components to share this behivor

--- src/API/userRepos.js
+++ src/API/userRepos.js
@@ -1,6 +1,18 @@
 const userResult = async (username) => {
+    
+    if(!username) {
+        return 
+    };
     const url = new URL(`https://api.github.com/users/${username}/repos`)
     url.searchParams.set('sort', 'created');
     url.searchParams.set('direction', 'desc');
     url.searchParams.set('per_page', '10')
+
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            throw new Error(`Failed request ${response.status}`)
+        }
+
+        return response.json();
 }
