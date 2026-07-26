b89903a2d1472a852dc272aed147bbc79889e70d
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-25 23:07:15 -0500

    Done styling ul in userStats gotta add div and breakpoints

--- src/components/Results/UserStats/UserStats.jsx
+++ src/components/Results/UserStats/UserStats.jsx
@@ -7,30 +7,40 @@ import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 const UserStats = ({followers, following, repos, gists, org}) => {
     return (
         <ul className="flex p-4 justify-around bg-slate-50 border border-digital-blue-100 rounded-md shadow-sm">
-            <li>
-                <FontAwesomeIcon icon={faUsers} />
-                <p>{followers}</p>
-                <p>Followers</p>
+            <li className="flex items-center gap-4">
+                <FontAwesomeIcon icon={faUsers} className="text-digital-blue-400 text-2xl pb-2"/>
+                <div>
+                    <p className="font-bold text-xl">{followers}</p>
+                    <p className="text-gray-400 text-sm">Followers</p>
+                </div>
             </li>
-            <li>
-                <FontAwesomeIcon icon={faUsers} />
-                <p>{following}</p>
-                <p>Following</p>
+            <li className="flex items-center gap-4">
+                <FontAwesomeIcon icon={faUsers} className="text-purple-400 text-2xl pb-2"/>
+                <div>
+                    <p className="font-bold text-xl">{following}</p>
+                    <p className="text-gray-400 text-sm">Following</p>
+                </div>
             </li>
-            <li>
-                <FontAwesomeIcon icon={faCodeBranch} />
-                <p>{repos}</p>
-                <p>Repositories</p>
+            <li className="flex items-center gap-4">
+                <FontAwesomeIcon icon={faCodeBranch} className="text-pink-400 text-2xl pb-2"/>
+                <div>
+                <p className="font-bold text-xl">{repos}</p>
+                <p className="text-gray-400 text-sm">Repositories</p>
+                </div>
             </li>
-            <li>
-                <FontAwesomeIcon icon={faStar} />
-                <p>{gists}</p>
-                <p>Gists</p>
+            <li className="flex items-center gap-4">
+                <FontAwesomeIcon icon={faStar} className="text-yellow-400 text-2xl pb-2"/>
+                <div>
+                <p className="font-bold text-xl">{gists}</p>
+                <p className="text-gray-400 text-sm">Gists</p>
+                </div>
             </li>
-            <li>
-                <FontAwesomeIcon icon={faBuilding} />
-                <p>{org || 'N/A'}</p>
-                <p>Organizations</p>
+            <li className="flex items-center gap-4">
+                <FontAwesomeIcon icon={faBuilding} className="text-green-400 text-2xl pb-2"/>
+                <div>
+                <p className="font-bold text-xl">{org || 'N/A'}</p>
+                <p className="text-gray-400 text-sm">Organizations</p>
+                </div>
             </li>
         </ul>
     );
