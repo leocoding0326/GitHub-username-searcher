89bcbb6bd8ea769604e3eea90bcc7d30d6332388
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-26 21:57:19 -0500

    Decided to do an overview section so removed the param requesting only 10 repos in my api call

--- src/API/userRepos.js
+++ src/API/userRepos.js
@@ -6,7 +6,6 @@ const userRepos = async (username) => {
     const url = new URL(`https://api.github.com/users/${username}/repos`)
     url.searchParams.set('sort', 'created');
     url.searchParams.set('direction', 'desc');
-    url.searchParams.set('per_page', '10')
 
         const response = await fetch(url);
 
