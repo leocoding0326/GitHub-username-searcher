9a26acfaf8da71ecd52419a2f1740e4a51af351a
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-27 17:41:36 -0500

    Handler function created to update my navChoice state:
    it listens for the event target text content,
    used toLowercase so the text allways match and used trim to avoid white space

--- src/components/Results/UserRepos/UserRepos.jsx
+++ src/components/Results/UserRepos/UserRepos.jsx
@@ -2,6 +2,11 @@ import ReposMenu from "./ReposMenu";
 
 const [navChoice, setNavChoice] = useState('overview');
 
+const handleNavChoice = (e) => {
+    const tab = e.currentTarget.textContent.trim().tolowerCase();
+    setNavChoice(tab);
+};
+
 const UserRepos = () => {
     return (
         <div>
