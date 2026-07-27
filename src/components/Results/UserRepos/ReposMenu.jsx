3e16f403a1fe22280833173741b9bd8bf25ab845
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-27 16:34:03 -0500

    Ul list created inside ReposMenu: set buttons inside li to keep semmantics

--- src/components/Results/UserRepos/ReposMenu.jsx
+++ src/components/Results/UserRepos/ReposMenu.jsx
@@ -1,5 +1,15 @@
 const ReposMenu = () => {
-
+    <ul>
+        <li>
+            <button>Overview</button>
+        </li>
+        <li>
+            <button>Repositories</button>
+        </li>
+        <li>
+            <button>Pinned Repos</button>
+        </li>
+    </ul>
 }
 
 export default ReposMenu;
