c2429f99d75bcfc63a69dc241d2a1882e4832daa
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-27 17:06:49 -0500

    Custom layer created to style tab menu, added hover effect to button so bg turns blue

--- src/components/Results/UserRepos/ReposMenu.jsx
+++ src/components/Results/UserRepos/ReposMenu.jsx
@@ -1,14 +1,14 @@
 const ReposMenu = () => {
     return (
-    <ul>
+    <ul className="flex gap-6 w-full tabs">
         <li>
-            <button>Overview</button>
+            <button className="tab-link hover:bg-digital-blue-50 transition-colors rounded-md">Overview</button>
         </li>
         <li>
-            <button>Repositories</button>
+            <button className="tab-link hover:bg-digital-blue-50 transition-colors rounded-md">Repositories</button>
         </li>
         <li>
-            <button>Pinned Repos</button>
+            <button className="tab-link hover:bg-digital-blue-50 transition-colors rounded-md">Pinned Repos</button>
         </li>
     </ul>
     );
