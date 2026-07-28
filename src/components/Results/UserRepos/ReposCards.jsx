100b02898b49380bdccf00b24357140b7a5e5b8c
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-27 22:00:35 -0500

    Vertical padding added to p in ReposCards

--- src/components/Results/UserRepos/ReposCards.jsx
+++ src/components/Results/UserRepos/ReposCards.jsx
@@ -8,7 +8,7 @@ const ReposCards = () => {
                 <h5 className='flex-1 ml-2'>Spoon-Knife</h5>
                 <div>Public</div>
             </div>
-            <p>This repo is for demonstration purposes only</p>
+            <p className='py-4'>This repo is for demonstration purposes only</p>
             <div>
                 <p>JavaScript</p>
                 <p>1.6k</p>
