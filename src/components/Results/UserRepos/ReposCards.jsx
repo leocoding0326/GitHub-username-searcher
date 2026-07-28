3a80ed09bd16acc484511ef5ac9931be61d01d6d
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-28 11:54:34 -0500

    Default text added to repos card p; color change to gray

--- src/components/Results/UserRepos/ReposCards.jsx
+++ src/components/Results/UserRepos/ReposCards.jsx
@@ -4,11 +4,11 @@ const ReposCards = ({name, description, forks, language, stars, visibility }) =>
     return (
         <div className="bg-slate-50 w-full h-full flex flex-col p-4 border border-digital-blue-100 shadow-sm rounded-md">
             <div className='flex items-center'>
-                <BookMarked />
-                <h5 className='flex-1 ml-2 text-lg font-bold'>{name}</h5>
+                <BookMarked className='text-gray-400'/>
+                <h5 className='flex-1 ml-2 text-lg font-bold text-digital-blue-700'>{name}</h5>
                 <div className='text-sm bg-green-600 px-2 py-1 rounded-xl text-white'>{visibility}</div>
             </div>
-            <p className='py-4 flex-1'>{description}</p>
+            <p className='py-4 flex-1 text-gray-500'>{description || 'User has not added a description'}</p>
             <div className='flex justify-around'>
                 <p className='flex items-center gap-1'><FileBraces size={20}/>{language}</p>
                 <p className='flex items-center gap-1'><Star size={20}/>{stars}</p>
