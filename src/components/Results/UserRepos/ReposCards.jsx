4c73d01e9cfea2e935810458735430db20c762da
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-27 22:24:00 -0500

    footer icons and test gap set to 1

--- src/components/Results/UserRepos/ReposCards.jsx
+++ src/components/Results/UserRepos/ReposCards.jsx
@@ -5,14 +5,14 @@ const ReposCards = () => {
         <div className="bg-slate-100 w-full h-auto flex flex-col p-4 border border-digital-blue-100 shadow-sm rounded-md">
             <div className='flex items-center'>
                 <BookMarked />
-                <h5 className='flex-1 ml-2 text-lg'>Spoon-Knife</h5>
+                <h5 className='flex-1 ml-2 text-lg font-bold'>Spoon-Knife</h5>
                 <div className='text-sm bg-green-600 px-2 py-1 rounded-xl text-white'>Public</div>
             </div>
             <p className='py-4'>This repo is for demonstration purposes only</p>
             <div className='flex justify-around'>
-                <p className='flex items-center'><FileBraces size={20}/> JavaScript</p>
-                <p className='flex items-center'><Star size={20}/>1.6k</p>
-                <p className='flex items-center'><GitFork size={20}/>300</p>
+                <p className='flex items-center gap-1'><FileBraces size={20}/> JavaScript</p>
+                <p className='flex items-center gap-1'><Star size={20}/>1.6k</p>
+                <p className='flex items-center gap-1'><GitFork size={20}/>300</p>
             </div>
         </div>
     );
