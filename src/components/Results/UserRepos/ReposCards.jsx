539606295df26bf4c049d40e5779edb9a1f290be
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-29 22:09:30 -0500

    Responsiviness issues on small screens

--- src/components/Results/UserRepos/ReposCards.jsx
+++ src/components/Results/UserRepos/ReposCards.jsx
@@ -9,7 +9,7 @@ const ReposCards = ({name, description, forks, language, stars, visibility }) =>
                 <div className='text-sm bg-green-600 px-2 py-1 rounded-xl text-white'>{visibility}</div>
             </div>
             <p className='py-6 flex-1 text-gray-500 flex items-center'>{description || 'User has not added a description'}</p>
-            <div className='flex justify-around'>
+            <div className='flex justify-around flex-wrap'>
                 <p className='flex items-center gap-1'><FileBraces size={20}/>{language}</p>
                 <p className='flex items-center gap-1'><Star size={20} className='text-yellow-500'/>{stars}</p>
                 <p className='flex items-center gap-1'><GitFork size={20} className='text-pink-400'/>{forks}</p>
