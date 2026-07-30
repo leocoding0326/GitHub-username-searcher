6410bf8d50eff43c262c8bb20f2a1700857d0baa
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-07-29 21:46:54 -0500

    Called UserBio into UserRepos

--- src/components/Results/UserRepos/UserRepos.jsx
+++ src/components/Results/UserRepos/UserRepos.jsx
@@ -21,9 +21,9 @@ const UserRepos = ({reposObject, resultObject}) => {
     return (
         <div className="p-4 border border-digital-blue-100 shadow-sm rounded-md bg-slate-50">
             <ReposMenu chooseTab = {handleNavChoice} current = {navChoice}/>
-            {navChoice==='overview' && <h4 className="p-4 text-xl border-b border-gray-200 font-bold">Top Repos</h4>}
-            {navChoice==='overview' && <RenderRepos object={overviewRepos}/>}
             {navChoice==='overview' && <UserBio resultObject={resultObject}/>}
+            {navChoice==='overview' && <h4 className="p-4 text-xl border-b border-gray-200 font-bold">Top Repos</h4>} 
+            {navChoice==='overview' && <RenderRepos object={overviewRepos}/>}
             {navChoice==='repositories' && <RenderRepos object={reposObject}/>}
         </div>
     );
