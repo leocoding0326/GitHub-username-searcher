4956050c2d2b23b6c0661f51e8165ec507d6d991
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-01 22:53:39 -0500

    Kept using ? to display diff message in Discover when promise returns 404

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -10,10 +10,13 @@ const Discover = ({popularArray, setSearch, setUsers, userNotFound}) => {
         <div className="md:w-[80%] w-[90%] flex flex-col items-center text-center justify-center shadow-md border-2 border-digital-blue-100 rounded-xl p-6 bg-slate-50">
             
             <SearchImage src={!userNotFound ? discoverImg : notFoundImg}/>
-            <DiscoverText />
-            <Divider />
-            <PopularSearches popularArray={popularArray} setSearch={setSearch} setUsers={setUsers}/>
-
+            <DiscoverText userNotFound={userNotFound}/>
+            {!userNotFound && (
+                <>
+                <Divider />
+                <PopularSearches popularArray={popularArray} setSearch={setSearch} setUsers={setUsers}/>
+                </>
+            )}
         </div>
     )
 }
--- src/components/Discover/DiscoverTexts.jsx
+++ src/components/Discover/DiscoverTexts.jsx
@@ -1,8 +1,8 @@
-const DiscoverText = () => {
+const DiscoverText = ({userNotFound}) => {
     return (
         <div className="mt-2 max-w-prose">
-            <h3 className="font-bold text-2xl mb-2">Start Your Search</h3>
-            <p>Enter a GitHub username above to find user profiles, repositories, statistics and more.</p>
+            <h3 className="font-bold text-2xl mb-2">{!userNotFound ? 'Start Your Search' : 'Sorry, cannot find this user'}</h3>
+            <p>{ !userNotFound ? 'Enter a GitHub username above to find user profiles, repositories, statistics and more.' : 'Please try a different username...'}</p>
         </div>
     );
 };
