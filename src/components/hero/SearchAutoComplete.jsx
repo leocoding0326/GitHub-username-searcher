31b7181cff2ccd7c4c788c35acb4a65b7f61cfea
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-01 22:55:51 -0500

    py-6 added to SearchAutoComplete

--- src/components/hero/SearchAutoComplete.jsx
+++ src/components/hero/SearchAutoComplete.jsx
@@ -85,7 +85,7 @@ const SearchAutoComplete = ({searchFunction, getLabel, clearResult, search, setS
     }, []);
 
     return (
-        <div ref={searchContainerRef} className='sm:h-[40vh] flex flex-col justify-center sm:min-w-[80%] mx-4'>
+        <div ref={searchContainerRef} className='sm:h-[40vh] flex flex-col justify-center sm:min-w-[80%] mx-4 py-6'>
             <HeroTitle />
             <div>
             <SearchBar 
