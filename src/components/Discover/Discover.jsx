d498f73217be7d98036511fbbe722551bced5fa4
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-01 22:42:34 -0500

    Tenary used to pass img prop src in depends if it is true or false

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -5,11 +5,11 @@ import PopularSearches from "./PopularSearches";
 import discoverImg from "../../assets/discoverImg.svg";
 import notFoundImg from "../../assets/notFoundImg.svg"
 
-const Discover = ({popularArray, setSearch, setUsers, userNotFund}) => {
+const Discover = ({popularArray, setSearch, setUsers, userNotFound}) => {
     return (
         <div className="md:w-[80%] w-[90%] flex flex-col items-center text-center justify-center shadow-md border-2 border-digital-blue-100 rounded-xl p-6 bg-slate-50">
             
-            <SearchImage src={discoverImg}/>
+            <SearchImage src={!userNotFound ? discoverImg : notFoundImg}/>
             <DiscoverText />
             <Divider />
             <PopularSearches popularArray={popularArray} setSearch={setSearch} setUsers={setUsers}/>
