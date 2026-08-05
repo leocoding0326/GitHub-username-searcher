d535a31e1ff16ac112e43e31d97c6d8fbffd9c2f
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-04 23:27:16 -0500

    Hook Form installed

--- src/components/hero/SearchForm.jsx
+++ src/components/hero/SearchForm.jsx
@@ -1,6 +1,13 @@
-const SearchForm = () => {
-    return (
+import ClearButton from "./ClearButton";
+import SearchButton from "./SearchButton";
 
+const SearchForm = ({}) => {
+    return (
+        <form>
+            <input type="text" />
+            <SearchButton />
+            <ClearButton />
+        </form>
     );
 };
 
