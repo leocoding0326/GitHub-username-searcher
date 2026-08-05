245c1ad497b4f1ed72ba39a2f5ac64d09c591b0d
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-04 23:32:38 -0500

    Deconstructed forms objects on var:
    invoqued register and use it to register input as username,
    made input required and set error msg to please enter an username

--- src/components/hero/SearchForm.jsx
+++ src/components/hero/SearchForm.jsx
@@ -1,10 +1,23 @@
 import ClearButton from "./ClearButton";
 import SearchButton from "./SearchButton";
+import { useForm } from "react-hook-form";
 
-const SearchForm = ({}) => {
+const SearchForm = () => {
+
+    const {
+        register,
+        handleSubmit
+    } = useForm()
+
+
     return (
         <form>
-            <input type="text" />
+            <input type="text" 
+            {...register(
+                'username',{
+                required: 'Please enter a valid username',
+                }
+            )}/>
             <SearchButton />
             <ClearButton />
         </form>
