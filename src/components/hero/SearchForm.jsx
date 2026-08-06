dfe553c65bcdf3796780fdde0072794483a0bd6b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-05 22:43:24 -0500

    Cleared blank spaces for redability

--- src/components/hero/SearchForm.jsx
+++ src/components/hero/SearchForm.jsx
@@ -33,12 +33,9 @@ const SearchForm = ({onSubmit}) => {
 
             <div className="flex w-full gap-2 items-center">
                     <input type="text" 
-                    {...register(
-                        'username',{
-                        required: 'This field cannot be empty',
-                        }
-                    )}
-                    className={`border ${errors.username ? 'border-red-500' : 'border-gray-300'} rounded-md px-2 py-1 focus:outline-digital-blue-200 focus:outline-1 w-full bg-slate-50 flex-1`}/>
+                    {...register('username')}
+                    className={`border ${errors.username ? 'border-red-500' : 'border-gray-300'} rounded-md px-2 py-1 focus:outline-digital-blue-200 focus:outline-1 w-full bg-slate-50 flex-1`}
+                    />
 
                     <SearchButton isSubmitting={isSubmitting}/>
 
