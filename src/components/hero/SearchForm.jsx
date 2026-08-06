8e37d31b611e081e7f08630a290f454438f92edc
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-05 22:12:06 -0500

    Added space for readibility

--- src/components/hero/SearchForm.jsx
+++ src/components/hero/SearchForm.jsx
@@ -21,6 +21,7 @@ const SearchForm = ({onSubmit}) => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="relative flex flex-col gap-1 p-2 justify-center">
+
             <div className="flex w-full gap-2 items-center">
                     <input type="text" 
                     {...register(
@@ -29,9 +30,12 @@ const SearchForm = ({onSubmit}) => {
                         }
                     )}
                     className={`border ${errors.username ? 'border-red-500' : 'border-gray-300'} rounded-md px-2 py-1 focus:outline-digital-blue-200 focus:outline-1 w-full bg-slate-50 flex-1`}/>
+
                     <SearchButton isSubmitting={isSubmitting}/>
+                    
                     {watch('username') && <ClearButton onClear = {() => reset()}/>}
             </div>
+
                 {!errors.username 
                     ? <Tip /> 
                     : <div className="text-red-500 text-xs flex gap-1 items-center"><TriangleAlert size={13}/>{errors.username.message}</div>}
