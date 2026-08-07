d0c8688e87c2963cc40a63af08c50e3289c5857f
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-06 23:41:07 -0500

    Installed combobox and exported it to form

---
+++ src/components/hero/AutoComplete.jsx
@@ -0,0 +1,11 @@
+import { Combobox } from "@base-ui/react";
+
+const AutoComplete = () => {
+    return (
+        <Combobox.Root>
+            <Combobox.Input />
+        </Combobox.Root>
+    )
+}
+
+export default AutoComplete;
