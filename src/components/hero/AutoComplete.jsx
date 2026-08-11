2b6cb6156226ef311815f8c220081895fecb5b7f
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-11 18:11:04 -0500

    Started working in my combobox:
    added root and input

--- src/components/hero/AutoComplete.jsx
+++ src/components/hero/AutoComplete.jsx
@@ -1,9 +1,48 @@
 import { Combobox } from "@base-ui/react";
+import { useState } from "react";
+import userSearch from "@/api/userSearch";
 
 const AutoComplete = () => {
+
+const [users, setUsers] = useState([]);
+
+const debouncedSearch = useMemo(
+    () => 
+        debounce(async (value) => {
+            try {
+                const results = await userSearch(value);
+                setUsers(results)
+            }
+            catch(error) {
+                console.log(`Something went wrong ${error}`)
+            }
+        }, 600),
+        [userSearch]
+);
+
+useEffect(() => {
+    return () => {
+        debouncedSearch.cancel();
+    };
+}, [debouncedSearch]);
+
+
+
     return (
-        <Combobox.Root>
-            <Combobox.Input />
+        <Combobox.Root 
+            items={users}
+            itemToStringValue={(user) => user.login}>
+
+            <Combobox.Input placeholder="Enter the username..."
+            onValueChange= {(value) => {
+                if(!value) {
+                    debouncedSearch.cancel();
+                    return
+                }
+                debouncedSearch(value);
+            }}
+            />
+
         </Combobox.Root>
     )
 }
