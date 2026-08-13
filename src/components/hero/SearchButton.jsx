55b874d938961a8638c9648d75e764ce238c3f8b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-12 20:18:06 -0500

    Matched p of input in button

--- src/components/hero/SearchButton.jsx
+++ src/components/hero/SearchButton.jsx
@@ -3,7 +3,7 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchButton = ({isSubmitting}) => {
     return  (
-        <button disabled={isSubmitting} type='submit' className='bg-digital-blue-500 rounded-md px-2 py-1 text-white hover:bg-digital-blue-600 cursor-pointer'>
+        <button disabled={isSubmitting} type='submit' className='bg-digital-blue-500 rounded-md p-2 text-white hover:bg-digital-blue-600 cursor-pointer'>
             <FontAwesomeIcon icon={faMagnifyingGlass}/>
             <span className='min-[375px]:inline hidden'> {isSubmitting ? 'Loading' : 'Search'}</span>
         </button>
