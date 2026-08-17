f92cd76bb4a3cf02bf88cc5728ebb8c949e39c52
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-16 21:25:15 -0500

    Breakpoint added to EmptyBioError img:
    breakpoint in sm screen so it is only w-20

--- src/components/Results/UserRepos/EmptyBioError.jsx
+++ src/components/Results/UserRepos/EmptyBioError.jsx
@@ -3,7 +3,7 @@ import noBio from '../../../assets/noBio.svg';
 const EmptyBio = () => {
     return (
         <div className=" p-6 flex flex-col justify-center items-center m-auto gap-6">
-            <div className='w-40 h-auto'>
+            <div className='sm:w-40 w-20 h-auto'>
                 <img src={noBio} alt="user bio not fund" />
             </div>
             <p className='text-gray-400'>User has not enter a Bio yet...</p>
