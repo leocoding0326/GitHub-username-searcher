6cb737e5ae1aa6125be3569eb494c100324e1ce9
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 17:15:45 -0500

    For better UI i removed logo from Hero section

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -1,10 +1,7 @@
-import Logo from '../../../NavBar/Logo';
-import logo from '../../../../assets/logo.svg'
-
 const AboutHero = () => {
     return (
-        <section>
-            <Logo size={'lg'} imgSrc={logo} alt='Logo of Github Finder'/>
+        <section className='p-4 text-center h-80 flex items-center justify-center'>
+            <h2 className='text-3xl font-bold max-w-[80%] leading-relaxed'>Discover GitHub profiles and repositories in seconds.<br/>Search, explore, and learn about developers and their work.</h2>
         </section>
     )
 }
