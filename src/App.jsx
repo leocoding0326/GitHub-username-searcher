d7a51bb3259de14abf185899c1a1a8972b55fa5a
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-17 21:41:33 -0500

    Imported and added routes for my new files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/routes/Home/Home';
+import About from './components/routes/About/About';
+import Contact from './components/routes/Contact/Contact';
+import Features from './components/routes/Features/Features';
 
 
 
@@ -11,6 +14,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>}></Route>
+      <Route path='about' element={<About/>}></Route>
+      <Route path='contact' element={<Contact/>}></Route>
+      <Route path='features' element={<Features/>}></Route>
     </Route>
   )
 );
