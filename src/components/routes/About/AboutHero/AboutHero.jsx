7c553fd4f57ea32c3aae36a8da2ce455e4728cf6
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 16:54:52 -0500

    Logo added to hero section of about:
    have to adjust color and img

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -1,5 +1,11 @@
+import Logo from '../../../NavBar/Logo';
+
 const AboutHero = () => {
-    
+    return (
+        <section>
+            <Logo size={'lg'}/>
+        </section>
+    )
 }
 
 export default AboutHero
