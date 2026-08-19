c33c927170ecfc3eba3c8aff3aee8051a327b02b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 21:38:18 -0500

    Set cards background to digital blue 700

--- src/components/routes/About/AboutHero/AboutCards.jsx
+++ src/components/routes/About/AboutHero/AboutCards.jsx
@@ -3,28 +3,28 @@ import { UserSearch } from 'lucide-react';
 const AboutCards = () => {
     return (
     <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4'>
-        <div>
+        <div className='bg-digital-blue-700'>
             <div className='flex gap-4'>
                 <UserSearch className='bg-blue-600'/>
                 <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
             <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
         </div>
-        <div>
+        <div className='bg-digital-blue-700'>
             <div className='flex gap-4'>
                 <UserSearch className='bg-blue-600'/>
                 <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
             <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
         </div>
-            <div>
-                <div className='flex gap-4'>
-                    <UserSearch className='bg-blue-600'/>
-                    <h3 className='text-xl font-bold'>Find Developers</h3>
-                </div>
-                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+        <div className='bg-digital-blue-700'> 
+            <div className='flex gap-4'>
+                <UserSearch className='bg-blue-600'/>
+                <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
+            <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
         </div>
+    </div>
     );
 };
 
