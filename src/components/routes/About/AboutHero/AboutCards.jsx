66a844ddf730e9377ad5a4dbe7938375d947af62
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 22:17:17 -0500

    None

--- src/components/routes/About/AboutHero/AboutCards.jsx
+++ src/components/routes/About/AboutHero/AboutCards.jsx
@@ -1,29 +1,30 @@
 import { UserSearch } from 'lucide-react';
-import Divider from '@/components/Divider';
+import { CodeXml } from 'lucide-react';
+import { TrendingUp } from 'lucide-react';
 
 const AboutCards = () => {
     return (
-    <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4  text-slate-300'>
-        <div className='bg-linear-to-b from-digital-blue-800 to-digital-blue-600 p-6 flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
+    <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4  text-slate-300'>
+        <div className='bg-linear-to-l from-digital-blue-900 to-digital-blue-800 p-6 flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
             <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
                 <UserSearch className='bg-blue-500 p-2 rounded' size={40}/>
                 <h3 className='md:text-2xl text-xl font-bold'>Find Developers</h3>
             </div>
             <p className='md:text-lg text-md leading-relaxed'>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
         </div>
-        <div className='bg-digital-blue-700'>
-            <div className='flex gap-4'>
-                <UserSearch className='bg-blue-600'/>
-                <h3 className='text-xl font-bold'>Find Developers</h3>
+        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 px-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
+            <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
+                <CodeXml className='bg-pink-700 p-2 rounded' size={40}/>
+                <h3 className='text-xl font-bold'>Explore Repos</h3>
             </div>
-            <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+            <p className='leading-relaxed'>Browse public repositories with language info, star counts, and descriptions. Filter by tech stack and popularity.</p>
         </div>
-        <div className='bg-digital-blue-700'> 
-            <div className='flex gap-4'>
-                <UserSearch className='bg-blue-600'/>
-                <h3 className='text-xl font-bold'>Find Developers</h3>
+        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 px-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
+            <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
+                <TrendingUp className='bg-emerald-700 p-2 rounded' size={40}/>
+                <h3 className='text-xl font-bold'>Quick Stats</h3>
             </div>
-            <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+            <p className='leading-relaxed'>Get real-time GitHub data including followers, following, public repos, organizations, and latest gists.</p>
         </div>
     </div>
     );
--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -2,9 +2,10 @@ import heroImg from '../../../../assets/aboutHeroGif.gif'
 
 const AboutHero = () => {
     return (
-        <section className='text-center md:h-[40vh] h-[30vh] flex flex-col gap-2 items-center justify-center p-4'>
+        <section className='text-center md:h-[60vh] h-[30vh] flex flex-col items-center justify-center p-4 gap-6'>
             <h2 className='md:text-3xl text-md font-bold leading-relaxed'>Search, explore, and learn about developers and their work.</h2>
-            <img src={heroImg} className='shadow-md rounded-md h-auto max-w-[80%] border border-digital-blue-100'/>
+            <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
+            wide-full border border-digital-blue-100'/>
         </section>
     )
 }
