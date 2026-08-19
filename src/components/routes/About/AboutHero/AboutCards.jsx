6ad12a98bc0fc6eb5841e468f641130e2bd8c06a
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 21:36:20 -0500

    grid minmax used for cards layout

--- src/components/routes/About/AboutHero/AboutCards.jsx
+++ src/components/routes/About/AboutHero/AboutCards.jsx
@@ -2,26 +2,26 @@ import { UserSearch } from 'lucide-react';
 
 const AboutCards = () => {
     return (
-        <div className='bg-digital-blue-700 p-4'>
+    <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4'>
+        <div>
             <div className='flex gap-4'>
                 <UserSearch className='bg-blue-600'/>
-                <h3 className='text-lg font-bold'>Find Developers</h3>
+                <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
-            <div>
-                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
-            </div>
+            <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+        </div>
+        <div>
             <div className='flex gap-4'>
                 <UserSearch className='bg-blue-600'/>
-                <h3 className='text-lg font-bold'>Find Developers</h3>
+                <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
+            <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+        </div>
             <div>
-                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
-            </div>
-            <div className='flex gap-4'>
-                <UserSearch className='bg-blue-600'/>
-                <h3 className='text-lg font-bold'>Find Developers</h3>
-            </div>
-            <div>
+                <div className='flex gap-4'>
+                    <UserSearch className='bg-blue-600'/>
+                    <h3 className='text-xl font-bold'>Find Developers</h3>
+                </div>
                 <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
             </div>
         </div>
