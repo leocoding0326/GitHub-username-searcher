0e1aaaf4b2b500a51296bed561c18bd4149d56d0
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-18 21:29:05 -0500

    Cards for component created:
    added an icon and h3 on each,
    styled background,
    set flex container for title and icon

--- src/components/routes/About/AboutHero/AboutCards.jsx
+++ src/components/routes/About/AboutHero/AboutCards.jsx
@@ -2,9 +2,27 @@ import { UserSearch } from 'lucide-react';
 
 const AboutCards = () => {
     return (
-        <div>
+        <div className='bg-digital-blue-700 p-4'>
+            <div className='flex gap-4'>
+                <UserSearch className='bg-blue-600'/>
+                <h3 className='text-lg font-bold'>Find Developers</h3>
+            </div>
             <div>
-            <UserSearch />
+                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+            </div>
+            <div className='flex gap-4'>
+                <UserSearch className='bg-blue-600'/>
+                <h3 className='text-lg font-bold'>Find Developers</h3>
+            </div>
+            <div>
+                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+            </div>
+            <div className='flex gap-4'>
+                <UserSearch className='bg-blue-600'/>
+                <h3 className='text-lg font-bold'>Find Developers</h3>
+            </div>
+            <div>
+                <p>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
             </div>
         </div>
     );
