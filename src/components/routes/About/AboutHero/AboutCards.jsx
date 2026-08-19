a5ccdf33fdf268a5c50ebdbb179952ad3f4af771
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-19 00:42:16 -0500

    Also adjusted colors in about page

--- src/components/routes/About/AboutHero/AboutCards.jsx
+++ src/components/routes/About/AboutHero/AboutCards.jsx
@@ -5,22 +5,22 @@ import { TrendingUp } from 'lucide-react';
 const AboutCards = () => {
     return (
     <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4  text-slate-300'>
-        <div className='bg-linear-to-l from-digital-blue-900 to-digital-blue-800 p-6 flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
-            <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
+        <div className='bg-linear-to-l from-digital-blue-900 to-digital-blue-800 p-6 flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-800'>
+            <div className='flex gap-4 items-center border-b pb-4 border-digital-blue-800'>
                 <UserSearch className='bg-blue-500 p-2 rounded' size={40}/>
-                <h3 className='md:text-2xl text-xl font-bold'>Find Developers</h3>
+                <h3 className='text-xl font-bold'>Find Developers</h3>
             </div>
-            <p className='md:text-lg text-md leading-relaxed'>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
+            <p className='leading-relaxed'>Search GitHub by username and instantly access profiles with follower counts, repositories, and contribution stats.</p>
         </div>
-        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 px-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
-            <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
+        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 p-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-800'>
+            <div className='flex gap-4 items-center border-b pb-4 border-digital-blue-800'>
                 <CodeXml className='bg-pink-700 p-2 rounded' size={40}/>
                 <h3 className='text-xl font-bold'>Explore Repos</h3>
             </div>
-            <p className='leading-relaxed'>Browse public repositories with language info, star counts, and descriptions. Filter by tech stack and popularity.</p>
+            <p className='leading-relaxed'>Browse public repositories with language info, star counts, and descriptions. Filter by language, stack and much more.</p>
         </div>
-        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 px-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-100'>
-            <div className='flex gap-4 items-center border-b pb-4 border-gray-500'>
+        <div className='bg-linear-to-r from-digital-blue-900 to-digital-blue-800 p-6  justify-center flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-800'>
+            <div className='flex gap-4 items-center border-b pb-4 border-digital-blue-800'>
                 <TrendingUp className='bg-emerald-700 p-2 rounded' size={40}/>
                 <h3 className='text-xl font-bold'>Quick Stats</h3>
             </div>
--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -2,8 +2,8 @@ import heroImg from '../../../../assets/aboutHeroGif.gif'
 
 const AboutHero = () => {
     return (
-        <section className='text-center md:h-[60vh] h-[30vh] flex flex-col items-center justify-center p-4 gap-6'>
-            <h2 className='md:text-3xl text-md font-bold leading-relaxed'>Search, explore, and learn about developers and their work.</h2>
+        <section className='text-center h-100 flex flex-col items-center justify-center p-4 gap-6'>
+            <h2 className='sm:text-3xl text-lg font-bold leading-relaxed text-white'>Search, explore, and learn about developers and their work.</h2>
             <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
             wide-full border border-digital-blue-100'/>
         </section>
