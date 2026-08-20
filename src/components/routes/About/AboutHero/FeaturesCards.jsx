a7cfa84cb0660ddb57d4e6cd13dd0e213e0d9e49
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:08:25 -0500

    Replaced all info to match banners

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -2,12 +2,28 @@ import { Zap,ShieldMinus,TabletSmartphone } from 'lucide-react'
 
 const FeaturesCards = () => {
     return (
-        <div className='flex py-10 flex-col'>
+        <div className='flex py-10 flex-col gap-10'>
             <div className='flex gap-4'>
                 <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
                 <div className='text-start text-sm flex flex-col gap-1'>
                     <h3 className='text-white font-bold'>Instant Result</h3>
                     <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
+                </div>
+            </div>
+
+            <div className='flex gap-4'>
+                <ShieldMinus className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+                <div className='text-start text-sm flex flex-col gap-1'>
+                    <h3 className='text-white font-bold'>Clean & Minimal</h3>
+                    <p className='text-gray-400'>Focus in what matters with a distraction free UI.</p>
+                </div>
+            </div>
+
+            <div className='flex gap-4'>
+                <TabletSmartphone className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+                <div className='text-start text-sm flex flex-col gap-1'>
+                    <h3 className='text-white font-bold'>Fully Responsive</h3>
+                    <p className='text-gray-400'>All features work in all devices</p>
                 </div>
             </div>
         </div>
