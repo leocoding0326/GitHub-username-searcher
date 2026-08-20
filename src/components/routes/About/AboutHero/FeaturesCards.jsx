7b99cd4fb2a0979acef8e3b1c326e9a2e4d64050
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:01:25 -0500

    Use flex to align the items

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -2,11 +2,11 @@ import { Zap,ShieldMinus,TabletSmartphone } from 'lucide-react'
 
 const FeaturesCards = () => {
     return (
-        <div>
+        <div className='flex gap-4 py-10'>
             <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
-            <div className='text-white text-start text-sm'>
-                <h3>Instant Result</h3>
-                <p>Get real-time data from the GitHub API.</p>
+            <div className='text-start text-sm flex flex-col gap-1'>
+                <h3 className='text-white font-bold'>Instant Result</h3>
+                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
             </div>
         </div>
     );
