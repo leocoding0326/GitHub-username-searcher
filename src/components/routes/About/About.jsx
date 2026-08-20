4e7a903d9bcb841549428e51749917c4fbea6563
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:34:48 -0500

    Changed Gif img in AboutHero:
    also added gradient to that section

--- src/components/routes/About/About.jsx
+++ src/components/routes/About/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <div className="max-w-360 w-full flex flex-col items-center gap-4 mx-auto">
             <AboutHero/>
-            <h2 className="text-3xl text-white">How it Works...</h2>
-            <AboutCards/>
+            {/*<h2 className="text-3xl text-white">How it Works...</h2>
+            <AboutCards/>*/}
         </div>
     );
 };
