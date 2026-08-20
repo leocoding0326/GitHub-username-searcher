229233b65b52dbd1a2d62c92cbbe6e42db01e237
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 10:41:12 -0500

    AboutHero chaged from flex to grid:
    set 2 cols for long screens 1 col for smallers

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -2,11 +2,13 @@ import heroImg from '../../../../assets/aboutHeroGif.gif';
 
 const AboutHero = () => {
     return (
-        <section className='text-center h-100 flex flex-col items-center justify-center p-4 gap-6'>
-            <h2 className='text-3xl font-bold leading-relaxed text-white'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
+        <section className='text-center h-100 grid grid-cols-1 sm:grid-cols-2 p-4 gap-6 items-center justify-center'>
+            <div className='flex flex-col gap-4'>
+                <h2 className='text-3xl font-bold leading-relaxed text-white'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
+                <p className='text-white line-clamp-3'>Github Finder is a fast and beautiful tool that lets you search for any GitHub username and inntantly explore their profile, repositories, stats and more...</p>
+            </div>
             <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
-            wide-full border border-digital-blue-100'/>
-            <p className='text-white line-clamp-3'>Github Finder is a fast and beautiful tool that lets you search for any GitHub username and inntantly explore their profile, repositories, stats and more...</p>
+            wide-full border border-digital-blue-100 mx-auto'/>
         </section>
     )
 }
