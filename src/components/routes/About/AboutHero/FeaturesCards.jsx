a496b6eb1810c0865eb58fa6e45954ff09c55c79
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 10:56:14 -0500

    Changes made to Zap icon:
    1 padding added,
    adjusted size to 40,

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -3,7 +3,7 @@ import { Zap,ShieldMinus,TabletSmartphone } from 'lucide-react'
 const FeaturesCards = () => {
     return (
         <div>
-            <Zap className='text-digital-blue-400' size={50}/>
+            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1' size={40}/>
             <div>
                 <h3>Instant Result</h3>
                 <p>Get real-time data from the GitHub API.</p>
