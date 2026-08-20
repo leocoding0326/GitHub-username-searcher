f60cb01acb5eb3e5e6b64f04e17ef16da36b55e7
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:01:59 -0500

    Copied and pasted zap to

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -8,6 +8,16 @@ const FeaturesCards = () => {
                 <h3 className='text-white font-bold'>Instant Result</h3>
                 <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
             </div>
+            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+            <div className='text-start text-sm flex flex-col gap-1'>
+                <h3 className='text-white font-bold'>Instant Result</h3>
+                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
+            </div>
+            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+            <div className='text-start text-sm flex flex-col gap-1'>
+                <h3 className='text-white font-bold'>Instant Result</h3>
+                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
+            </div>
         </div>
     );
 };
