3fd45d2d7c02d4141e60d4ff38de0aa44c68d592
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 10:32:29 -0500

    New text added to about hero and color set to white

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -3,9 +3,10 @@ import heroImg from '../../../../assets/aboutHeroGif.gif';
 const AboutHero = () => {
     return (
         <section className='text-center h-100 flex flex-col items-center justify-center p-4 gap-6'>
-            <h2 className='sm:text-3xl text-3xl font-bold leading-relaxed text-white'>Search and learn about developers and <span className='text-digital-blue-500'>their work.</span></h2>
+            <h2 className='text-3xl font-bold leading-relaxed text-white'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
             <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
             wide-full border border-digital-blue-100'/>
+            <p className='text-white line-clamp-3'>Github Finder is a fast and beautiful tool that lets you search for any GitHub username and inntantly explore their profile, repositories, stats and more...</p>
         </section>
     )
 }
