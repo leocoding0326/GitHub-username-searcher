55965c0fbbf052b04203cb6230f9bbf4d78cc30b
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:44:47 -0500

    Changed size of h2 to 4xl

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
     return (
         <section className='text-center min-h-screen grid grid-cols-1 sm:grid-cols-2 p-10 gap-6 items-center justify-center bg-linear-to-l from-digital-blue-500/40 to-transparent'>
             <div className='flex flex-col gap-6 items-start justify-center'>
-                <h2 className='text-3xl font-bold leading-relaxed text-white text-start'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
+                <h2 className='text-4xl font-bold leading-relaxed text-white text-start'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
                 <p className='text-white text-start leading-relaxed'>Github Finder is a fast and beautiful tool that lets you search for any username and inntantly explore their profile, repositories, stats and more...</p>
                 <FeaturesCards />
             </div>
