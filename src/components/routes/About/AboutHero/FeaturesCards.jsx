f49813f3ed2d0e107df3c832aac1ba650a0a1c35
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 10:57:52 -0500

    Changes made to FeaturesCards:
    text set to white,
    text size to sm

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -3,8 +3,8 @@ import { Zap,ShieldMinus,TabletSmartphone } from 'lucide-react'
 const FeaturesCards = () => {
     return (
         <div>
-            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1' size={40}/>
-            <div>
+            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+            <div className='text-white text-start text-sm'>
                 <h3>Instant Result</h3>
                 <p>Get real-time data from the GitHub API.</p>
             </div>
