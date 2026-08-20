4f7aa6a81655930038411d0fe094bc12378d71ff
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:04:59 -0500

    New container created to hold all cards

--- src/components/routes/About/AboutHero/FeaturesCards.jsx
+++ src/components/routes/About/AboutHero/FeaturesCards.jsx
@@ -2,21 +2,13 @@ import { Zap,ShieldMinus,TabletSmartphone } from 'lucide-react'
 
 const FeaturesCards = () => {
     return (
-        <div className='flex gap-4 py-10'>
-            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
-            <div className='text-start text-sm flex flex-col gap-1'>
-                <h3 className='text-white font-bold'>Instant Result</h3>
-                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
-            </div>
-            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
-            <div className='text-start text-sm flex flex-col gap-1'>
-                <h3 className='text-white font-bold'>Instant Result</h3>
-                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
-            </div>
-            <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
-            <div className='text-start text-sm flex flex-col gap-1'>
-                <h3 className='text-white font-bold'>Instant Result</h3>
-                <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
+        <div className='flex py-10 flex-col'>
+            <div className='flex gap-4'>
+                <Zap className='text-digital-blue-400 bg-digital-blue-700 p-1 rounded-md' size={40}/>
+                <div className='text-start text-sm flex flex-col gap-1'>
+                    <h3 className='text-white font-bold'>Instant Result</h3>
+                    <p className='text-gray-400'>Get real-time data from the GitHub API.</p>
+                </div>
             </div>
         </div>
     );
