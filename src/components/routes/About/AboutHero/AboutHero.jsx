a0e81d5aa6795a6f1f4a3e66740ca7c0a986a452
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 10:23:41 -0500

    Changed h2 size on About Hero to 3xl

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -1,9 +1,9 @@
-import heroImg from '../../../../assets/aboutHeroGif.gif'
+import heroImg from '../../../../assets/aboutHeroGif.gif';
 
 const AboutHero = () => {
     return (
         <section className='text-center h-100 flex flex-col items-center justify-center p-4 gap-6'>
-            <h2 className='sm:text-3xl text-lg font-bold leading-relaxed text-white'>Search, explore, and learn about developers and their work.</h2>
+            <h2 className='sm:text-3xl text-3xl font-bold leading-relaxed text-white'>Search and learn about developers and <span className='text-digital-blue-500'>their work.</span></h2>
             <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
             wide-full border border-digital-blue-100'/>
         </section>
