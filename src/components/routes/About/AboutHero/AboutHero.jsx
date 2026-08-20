f13e4d706de8c4915e0fb2721003ac730191fc16
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-20 11:12:10 -0500

    AboutHero text alignt to start:
    used items start and added text-start to p element

--- src/components/routes/About/AboutHero/AboutHero.jsx
+++ src/components/routes/About/AboutHero/AboutHero.jsx
@@ -3,10 +3,10 @@ import FeaturesCards from './FeaturesCards';
 
 const AboutHero = () => {
     return (
-        <section className='text-center h-100 grid grid-cols-1 sm:grid-cols-2 p-4 gap-6 items-center justify-center'>
-            <div className='flex flex-col gap-4'>
+        <section className='text-center h-100 grid grid-cols-1 sm:grid-cols-2 p-10 gap-6 items-center justify-center'>
+            <div className='flex flex-col gap-4 items-start'>
                 <h2 className='text-3xl font-bold leading-relaxed text-white'>Find any Github user in<span className='text-digital-blue-500'> seconds.</span></h2>
-                <p className='text-white line-clamp-3'>Github Finder is a fast and beautiful tool that lets you search for any GitHub username and inntantly explore their profile, repositories, stats and more...</p>
+                <p className='text-white text-start leading-relaxed'>Github Finder is a fast and beautiful tool that lets you search for any username and inntantly explore their profile, repositories, stats and more...</p>
                 <FeaturesCards />
             </div>
             <img src={heroImg} className='shadow-md rounded-md h-auto md:max-w-[80%]
