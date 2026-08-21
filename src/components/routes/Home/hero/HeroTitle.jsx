f3647e6f53c0856af2824dc095b8b0ba56aa8a73
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-21 10:11:39 -0500

    changed text again to 4xl

--- src/components/routes/Home/hero/HeroTitle.jsx
+++ src/components/routes/Home/hero/HeroTitle.jsx
@@ -1,7 +1,7 @@
 const HeroTitle = () => {
     return (
         <div className="space-y-2 mb-4 text-center text-white">
-            <h2 className="font-bold min-[450px]:text-5xl text-3xl tracking-wide">Search <span className="text-digital-blue-500">GitHub</span> Users</h2>
+            <h2 className="font-bold min-[450px]:text-5xl text-4xl tracking-wide">Search <span className="text-digital-blue-500">GitHub</span> Users</h2>
             <p>Find any GitHub user and explore their profile, repos and more.</p>
         </div>
     );
