52839ebd6f809cac73c0d3df2715808ebe7f60f0
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-08-21 09:58:05 -0500

    Title added to HowItWorks:
    added an h2 title and mod my current one to h3 to keep semmantics,
    set color of h2 to blue,
    addded breakpoint on sm screens to 2xl for responsiviness

--- src/components/routes/About/About.jsx
+++ src/components/routes/About/About.jsx
@@ -1,12 +1,12 @@
 import AboutHero from "./AboutHero/AboutHero";
-import AboutCards from "./AboutHero/HowItWorks";
+import HowItWorks from "./AboutHero/HowItWorks";
+
 
 const About = () => {
     return (
         <div className="max-w-360 w-full flex flex-col items-center gap-4 mx-auto">
             <AboutHero/>
-            <h2 className="text-3xl text-white">How it Works...</h2>
-            <AboutCards/>
+            <HowItWorks/>
         </div>
     );
 };
--- src/components/routes/About/AboutHero/HowItWorks.jsx
+++ src/components/routes/About/AboutHero/HowItWorks.jsx
@@ -2,9 +2,14 @@ import { UserSearch } from 'lucide-react';
 import { CodeXml } from 'lucide-react';
 import { TrendingUp } from 'lucide-react';
 
-const AboutCards = () => {
+const HowItWorks = () => {
     return (
-    <div className='p-4 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4  text-slate-300'>
+    <div className='py-10 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4  text-slate-300 justify-center items-center'>
+        <div className='col-span-full text-center py-2'>
+            <h2 className='text-digital-blue-500'>HOW IT WORKS</h2>
+            <h3 className="text-2xl text-white sm:text-3xl">Simple.Fast.Powerful.</h3>
+        </div>
+
         <div className='bg-linear-to-l from-digital-blue-900 to-digital-blue-800 p-6 flex flex-col gap-6 rounded-md shadow-md border border-digital-blue-800'>
             <div className='flex gap-4 items-center border-b pb-4 border-digital-blue-800'>
                 <UserSearch className='bg-blue-500 p-2 rounded' size={40}/>
@@ -30,4 +35,4 @@ const AboutCards = () => {
     );
 };
 
-export default AboutCards;
+export default HowItWorks;
